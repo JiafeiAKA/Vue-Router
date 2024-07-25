7cde6d32d0a809ec30f5430d3f33494e21a70899
JiafeiAKA <153718860+JiafeiAKA@users.noreply.github.com>
2024-07-25 23:30:18 +0700

    1.9 for real

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="{ name: 'Event'}">Event</RouterLink>
-          <RouterLink to="{ name: 'About' }">About</RouterLink>
-          <RouterLink to="{ name: 'Student' }">Student</RouterLink>
+          <RouterLink :to="{ name: 'event-list-view'}">Event</RouterLink>
+          <RouterLink :to="{ name: 'about' }">About</RouterLink>
+          <RouterLink :to="{ name: 'Student' }">Student</RouterLink>
         </nav>
       </div>
     </header>
