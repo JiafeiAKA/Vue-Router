2cedeb0312ade2cfbca869b33ae9b529060cd46d
JiafeiAKA <153718860+JiafeiAKA@users.noreply.github.com>
2024-07-25 22:42:47 +0700

    1.2

---
+++ src/views/EventDetailView.vue
@@ -0,0 +1,15 @@
+<script>
+import {ref, onMounted} from 'vue'
+import Event from '@/type/Event'
+const event = ref<Event>(null)
+onMounted(() => {
+    // fetch event (by id) and set local event data
+})
+</script>
+<template>
+    <div v-if="event">
+        <h1>{{  event.title }}</h1>
+        <p>{{ event.time }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
