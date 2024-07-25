a2a40777160aed18ff39c10ffc0dce512ca29463
JiafeiAKA <153718860+JiafeiAKA@users.noreply.github.com>
2024-07-25 23:24:28 +0700

    1.9

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Event</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-          <RouterLink to="/Student">Student</RouterLink>
+          <RouterLink to="{ name: 'Event'}">Event</RouterLink>
+          <RouterLink to="{ name: 'About' }">About</RouterLink>
+          <RouterLink to="{ name: 'Student' }">Student</RouterLink>
         </nav>
       </div>
     </header>
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import Event from '@/types/Event'
 defineProps<{
 event: Event
@@ -18,7 +17,7 @@ event: Event
 </script>
 
 <template>
-  <RouterLink to = "/event/5928101">
+  <RouterLink class="event-link" :to = "{ name: 'event-detail-view', params: {id: event.id }}">
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
@@ -39,5 +38,10 @@ event: Event
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-link{
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
