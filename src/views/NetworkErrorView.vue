04bf71daf2d324388eea98b356a089fe74074e18
JiafeiAKA <153718860+JiafeiAKA@users.noreply.github.com>
2024-08-07 21:32:26 +0700

    change db

--- src/views/NetworkErrorView.vue
+++ src/views/NetworkErrorView.vue
@@ -1,20 +1,6 @@
-<script setup lang="ts">
-import { defineProps, withDefaults} from 'vue'
-
-const props = withDefaults(
-    defineProps<{
-        resource: string 
-    }>(),
-    {
-        resource: 'page'
-    }
-
-)
-</script>
 <template>
     <div class="networkError">
         <h1>Uh-Oh!</h1>
-
         <h3>
             It's looks like you're experiencing some network issues, please 
             take a breath and 
